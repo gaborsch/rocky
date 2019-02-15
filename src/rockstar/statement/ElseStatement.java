9164728c211df9d3f5306e30e74c8bf84cc3c6eb
gaborsch <gabor.schermann@gmail.com>
2019-02-16 00:57:50 +0100

    Else lazy mode

--- src/rockstar/statement/ElseStatement.java
+++ src/rockstar/statement/ElseStatement.java
@@ -15,24 +15,24 @@ import rockstar.runtime.BlockContext;
  */
 public class ElseStatement extends Block implements ContinuingBlockStatementI {
 
-    @Override
-    boolean applyTo(Block block) {
-        List<Statement> blockStmts = block.getStatements();
-        if (blockStmts.size() >= 1) {
-            Statement lastStmt = blockStmts.get(blockStmts.size() - 1);
-            if (lastStmt instanceof IfStatement) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    @Override
+//    boolean applyTo(Block block) {
+//        List<Statement> blockStmts = block.getStatements();
+//        if (blockStmts.size() >= 1) {
+//            Statement lastStmt = blockStmts.get(blockStmts.size() - 1);
+//            if (lastStmt instanceof IfStatement) {
+//                return true;
+//            }
+//        }
+//        return false; // strict mode: ELSE only after IF
+//    }
 
     @Override
     public void appendTo(Block finishedBlock) {
         if (finishedBlock instanceof IfStatement) {
             ((IfStatement) finishedBlock).setElseStatement(this);
-        } else {
-            throw new ParseException("Else statement after " + finishedBlock.getClass().getSimpleName(), getLine());
+//        } else { // strict mode: ELSE only after IF
+//            throw new ParseException("Else statement after " + finishedBlock.getClass().getSimpleName(), getLine());
         }
     }
 
