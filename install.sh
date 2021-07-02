5ea992f468b08cdfc3ad63046981ddfb9245f747
Gabor Schermann <gabor.schermann@oracle.com>
2021-07-02 16:50:12 +0200

    Creating Linux runnable from jar file

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# make symlink
-rm -f /usr/bin/rockstar
-ln -s $PWD/rockstar /usr/bin/rockstar
+# create binary executable
+(echo '#!/bin/sh' && echo 'exec java -jar $0 "$@"' && cat rocky.jar) >rockstar.bin
+chmod +x rockstar.bin
 
+# move it to /usr/bin
+rm -f /usr/bin/rockstar
+mv rockstar.bin /usr/bin/rockstar
